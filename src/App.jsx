8ef742ac6ac186b429471ffe4df9a70a2d15abcd
harbanery <ryusuf05@gmail.com>
2024-07-17 15:55:37 +0700

    updating commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Fonts from "./styles/fonts";
 import { Container, Heading, Stack, Text } from "@chakra-ui/react";
 import { createBrowserRouter } from "react-router-dom";
@@ -9,7 +8,7 @@ import RootRouter from "./routes/RootRouter";
 
 function App() {
   return (
-    <RootRouter/>
+    <RootRouter />
     // <>
     //   <Fonts />
     //   <Container>

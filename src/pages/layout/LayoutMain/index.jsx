0dace620b5a4340343fad857fe9e980a809da794
syaifullohismail <syaifullohismail123@gmail.com>
2024-07-19 17:17:52 +0700

    feat: landing page, add search flight component

--- src/pages/layout/LayoutMain/index.jsx
+++ src/pages/layout/LayoutMain/index.jsx
@@ -1,12 +1,26 @@
+import { Button, Flex, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import SearchFlight from "../../../components/module/SearchFlightModal";
 
 const LayoutMain = () => {
   return (
     <>
-      <h1>Navbar</h1>
-      <Outlet />
-      <h1>Footer</h1>
+      <Flex>
+        <h1>Navbar</h1>
+        <Outlet />
+        <Button
+          _active={{
+            bg: null,
+            transform: null,
+          }}
+          bg="white"
+          _hover="none"
+        >
+        footer
+        </Button>
+        <SearchFlight />
+      </Flex>
     </>
   );
 };

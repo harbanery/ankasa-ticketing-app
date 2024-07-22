b334e5150d8be7b2042cd5425886dee1bc734719
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-07-22 19:41:43 +0700

    feat 2: browse page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Fonts from "./styles/fonts";
-import { Container, Heading, Stack, Text } from "@chakra-ui/react";
+import BrowsePage from './pages/main/Browse';
 
 function App() {
-  return (
-    <>
-      <Fonts />
-      <Container>
-        <Stack
-          fontFamily="myHeading"
-          fontSize={70}
-          fontWeight={100}
-          fontStyle={"italic"}
-        >
-          <Heading>ANKASA</Heading>
-          <Text>ANKASA</Text>
-          <Text fontFamily="myBody" fontSize={40} fontStyle={"normal"}>
-            ANKASA
-          </Text>
-        </Stack>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<BrowsePage />
+		</>
+	);
 }
 
 export default App;

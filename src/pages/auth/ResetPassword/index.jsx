0f48bd63ab5618ee64a32acee86919ae02c581fa
harbanery <ryusuf05@gmail.com>
2024-07-23 20:17:13 +0700

    feat 1: reset password page

--- src/pages/auth/ResetPassword/index.jsx
+++ src/pages/auth/ResetPassword/index.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 import { passwordValidation } from "../../../utils/validation";
-import { useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Collapse,
+  Container,
+  Flex,
+  FormControl,
+  Heading,
+  Image,
+  Input,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import api from "../../../services/api";
+import { Link, redirect, useSearchParams } from "react-router-dom";
 
 const ResetPassword = () => {
+  // const [searchParams] = useSearchParams();
+
   const toast = useToast();
   const [form, setForm] = useState({
     password: "",
@@ -45,7 +60,78 @@ const ResetPassword = () => {
     }
   };
 
-  return <div>ResetPassword</div>;
+  return (
+    <Container my="5%" maxW="sm">
+      <Link to={"/"}>
+        <Image
+          // display={{ base: "none", md: "flex" }}
+          position={"absolute"}
+          top={10}
+          src="/src/assets/brandicon.png"
+          alt="Logo"
+          w="158px"
+          h="36px"
+        />
+      </Link>
+      <Box display="flex" flexDirection="column" h="100%" gap="4">
+        <Heading fontFamily="Poppins" fontWeight="600">
+          Reset Password
+        </Heading>
+        <Flex
+          direction="column"
+          my="5"
+          gap="7"
+          fontFamily="Lato"
+          fontSize="16px"
+          fontWeight="400"
+        >
+          <FormControl isInvalid={errors.password ? true : false} isRequired>
+            <Input
+              type="password"
+              name="password"
+              value={form.password}
+              size="lg"
+              onChange={handleChange}
+              variant="flushed"
+              placeholder="Password"
+              borderBottom="2px"
+              borderBottomColor="#D2C2FFAD"
+              _focus={{
+                borderBottomColor: "#2395FF",
+              }}
+            />
+            <Collapse in={errors.password ? true : false} animateOpacity>
+              <Text fontSize="14px" mt="8px" textColor="crimson">
+                {errors.password || "."}
+              </Text>
+            </Collapse>
+          </FormControl>
+        </Flex>
+        <Button
+          onClick={handleForgotPassword}
+          bg="#2395FF"
+          borderRadius="10px"
+          color="white"
+          size="lg"
+          w="100%"
+          h="57px"
+          fontFamily="Poppins"
+          fontSize="18px"
+          fontWeight="700"
+          transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+          boxShadow="0px 8px 10px 0px #2395FF4D"
+          _hover={{ bg: "#1971c2" }}
+          _active={{
+            bg: "#dddfe2",
+            boxShadow: "0px 8px 10px 0px #dddfe24D",
+          }}
+        >
+          Send
+        </Button>
+      </Box>
+    </Container>
+  );
 };
+s;
 
 export default ResetPassword;
--- src/routes/RootRouter/index.jsx
+++ src/routes/RootRouter/index.jsx
@@ -1,59 +1,64 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Explore from '../../pages/main/Explore';
-import Login from '../../pages/auth/Login';
-import ForgotPassword from '../../pages/auth/ForgotPassword';
-import Register from '../../pages/auth/Register';
-import LayoutMain, { mainLoader } from '../../pages/layout/LayoutMain';
-import LayoutAuth, { authLoader } from '../../pages/layout/LayoutAuth';
-import FlightDetail from '../../pages/main/Flight';
-import BoardingPassPage from '../../pages/main/BoardingPass';
-import BrowsePage from '../../pages/main/Browse';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Explore from "../../pages/main/Explore";
+import Login from "../../pages/auth/Login";
+import ForgotPassword from "../../pages/auth/ForgotPassword";
+import Register from "../../pages/auth/Register";
+import LayoutMain, { mainLoader } from "../../pages/layout/LayoutMain";
+import LayoutAuth, { authLoader } from "../../pages/layout/LayoutAuth";
+import FlightDetail from "../../pages/main/Flight";
+import BoardingPassPage from "../../pages/main/BoardingPass";
+import BrowsePage from "../../pages/main/Browse";
+import ResetPassword from "../../pages/auth/ResetPassword";
 
 const RootRouter = () => {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <LayoutMain />,
-			loader: mainLoader,
-			children: [
-				{ index: true, element: <Explore /> },
-				{
-					path: 'browse',
-					element: <BrowsePage />,
-				},
-				{
-					path: 'flight/:id',
-					element: <FlightDetail />,
-				},
-				{
-					path: 'my-booking/:id',
-					element: <BoardingPassPage />,
-				},
-			],
-		},
-		{
-			path: '/auth',
-			element: <LayoutAuth />,
-			loader: authLoader,
-			children: [
-				{
-					path: 'login',
-					element: <Login />,
-				},
-				{
-					path: 'register',
-					element: <Register />,
-				},
-				{
-					path: 'forgot-password',
-					element: <ForgotPassword />,
-				},
-			],
-		},
-	]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LayoutMain />,
+      loader: mainLoader,
+      children: [
+        { index: true, element: <Explore /> },
+        {
+          path: "browse",
+          element: <BrowsePage />,
+        },
+        {
+          path: "flight/:id",
+          element: <FlightDetail />,
+        },
+        {
+          path: "my-booking/:id",
+          element: <BoardingPassPage />,
+        },
+      ],
+    },
+    {
+      path: "/auth",
+      element: <LayoutAuth />,
+      loader: authLoader,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+        {
+          path: "forgot-password",
+          element: <ForgotPassword />,
+        },
+        {
+          path: "reset-password",
+          element: <ResetPassword />,
+        },
+      ],
+    },
+  ]);
 
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default RootRouter;
